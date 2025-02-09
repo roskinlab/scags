8a913730791a95a097f3ce90a88a930b2a7ad9bd
Onyekachi Nwogu <kachibunny@gmail.com>
2025-02-09 17:03:40 -0500

    script guide on how to run Suppl heatmap

---
+++ lsf-jobs/do_Suppl_Figure_3_4_heatmap.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/bash
+
+# Steps to run Suppl_Figure3_4_heatmap.R code
+1. Make sure directory path were Suppl_Figure3_4_heatmap.R is stored has no spaces in the name
+2. Do Rscript Suppl_Figure3_4_heatmap.R path_to_file_data path_to_file_lineage
